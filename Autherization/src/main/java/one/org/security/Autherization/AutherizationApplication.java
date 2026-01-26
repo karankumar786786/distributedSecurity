b964f7c9bc1670559619784951dbb45f8f1d7b01
karankumar786786 <onestream6969@gmail.com>
2026-01-26 22:59:36 +0530

    feat: Initialize Spring Boot authorization service project with Maven wrapper and core application files.

---
+++ Autherization/src/main/java/one/org/security/Autherization/AutherizationApplication.java
@@ -0,0 +1,13 @@
+package one.org.security.Autherization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutherizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutherizationApplication.class, args);
+	}
+
+}
---
+++ Autherization/src/test/java/one/org/security/Autherization/AutherizationApplicationTests.java
@@ -0,0 +1,13 @@
+package one.org.security.Autherization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutherizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
